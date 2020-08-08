9fda7c8138d4c7ee96baccdb66b5e4ab7455efa5
Stefan Ziegler <edi.gonzales@gmail.com>
2020-08-08 07:30:35 +0200

    button

--- src/main/java/ch/so/agi/oereb/Main.java
+++ src/main/java/ch/so/agi/oereb/Main.java
@@ -1,6 +1,11 @@
 package ch.so.agi.oereb;
 
+import java.awt.Container;
 import java.awt.EventQueue;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class Main extends JFrame {
@@ -10,11 +15,36 @@ public class Main extends JFrame {
     }
     
     private void initUI() {     
-        setTitle("Simple example");
-        setSize(400, 200);
+        JButton quitButton = new JButton("Quit");
+
+        quitButton.addActionListener((event) -> System.exit(0));
+
+        createLayout(quitButton);
+
+        setTitle("Quit button");
+        setSize(300, 200);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
     }
+    
+    private void createLayout(JComponent... arg) {
+
+        Container pane = getContentPane();
+        GroupLayout gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+
+        gl.setAutoCreateContainerGaps(true);
+
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addComponent(arg[0])
+        );
+    }
+    
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
